ee4a0077afa366c9520461281ac254de60040595
Joao Morim <jmorimnascimento@ucf.edu>
2023-04-14 15:06:55 -0400

    new lesson

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-function TodoList(props) {
+function TodoList({todoList}) {
 
-return (
+    return (
         <ul>
-            {props.list.map((obj) => 
-            (<TodoListItem key={obj.id} vector={obj}/> /* vector defines what 'props name' we will use within todoListItem*/
+            {todoList.map((obj) => 
+            (<TodoListItem vec={obj} key={obj.id}/> /* vector defines what 'props name' we will use within todoListItem*/
             ))}
         </ul>
-        );
+    );
 }
 
 export default TodoList;
